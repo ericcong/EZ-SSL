fe7c1e077b37a5cafc6eac0719217a98abe115c1
Chen Cong <ericcong@gmail.com>
2018-04-15 20:05:56 -0400

    Add more constructors to match SocketFactory and ServerSocketFactory.

--- src/main/java/io/cong/chen/ezssl/SSLServerSocketBuilder.java
+++ src/main/java/io/cong/chen/ezssl/SSLServerSocketBuilder.java
@@ -4,29 +4,65 @@ import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLServerSocket;
 import javax.net.ssl.SSLServerSocketFactory;
 import java.io.IOException;
+import java.net.InetAddress;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
 
 public class SSLServerSocketBuilder extends
     AbstractSSLBuilder<SSLServerSocketBuilder> {
-  public SSLServerSocket build(int port, String sslProtocol)
-      throws NoSuchAlgorithmException, KeyManagementException, IOException {
+  public SSLServerSocket build(
+      Integer port, Integer backlog, InetAddress ifAddress, String sslProtocol)
+      throws NoSuchAlgorithmException,
+      KeyManagementException,
+      IOException,
+      IllegalArgumentException {
+
     SSLContext sslContext = SSLContext.getInstance(sslProtocol);
     sslContext.init(this.keyManagerList, this.trustManagerList, null);
 
     SSLServerSocketFactory sslServerSocketFactory =
         sslContext.getServerSocketFactory();
 
-    SSLServerSocket sslServerSocket =
-        (SSLServerSocket) sslServerSocketFactory.createServerSocket(port);
+    SSLServerSocket sslServerSocket = null;
+    if (port == null && backlog == null && ifAddress == null) {
+      sslServerSocket =
+          (SSLServerSocket) sslServerSocketFactory.createServerSocket();
+    } else if (port != null && backlog == null && ifAddress == null) {
+      sslServerSocket =
+          (SSLServerSocket) sslServerSocketFactory.createServerSocket(port);
+    } else if (port != null && backlog != null && ifAddress == null) {
+      sslServerSocket = (SSLServerSocket) sslServerSocketFactory
+          .createServerSocket(port, backlog);
+    } else if (port != null && backlog != null) {
+      sslServerSocket = (SSLServerSocket) sslServerSocketFactory
+          .createServerSocket(port, backlog, ifAddress);
+    } else {
+      throw new IllegalArgumentException();
+    }
+
     sslServerSocket.setEnabledCipherSuites(
         sslServerSocket.getSupportedCipherSuites());
 
     return sslServerSocket;
   }
 
+  public SSLServerSocket build()
+      throws NoSuchAlgorithmException, KeyManagementException, IOException {
+    return this.build(null, null, null, TLS);
+  }
+
   public SSLServerSocket build(int port)
       throws NoSuchAlgorithmException, KeyManagementException, IOException {
-    return this.build(port, TLS);
+    return this.build(port, null, null, TLS);
+  }
+
+  public SSLServerSocket build(int port, int backlog)
+      throws NoSuchAlgorithmException, KeyManagementException, IOException {
+    return this.build(port, backlog, null, TLS);
+  }
+
+  public SSLServerSocket build(int port, int backlog, InetAddress ifAddress)
+      throws NoSuchAlgorithmException, KeyManagementException, IOException {
+    return this.build(port, backlog, ifAddress, TLS);
   }
 }
--- src/main/java/io/cong/chen/ezssl/SSLSocketBuilder.java
+++ src/main/java/io/cong/chen/ezssl/SSLSocketBuilder.java
@@ -4,26 +4,93 @@ import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLSocket;
 import javax.net.ssl.SSLSocketFactory;
 import java.io.IOException;
+import java.net.InetAddress;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
 
 public class SSLSocketBuilder extends AbstractSSLBuilder<SSLSocketBuilder> {
-  public SSLSocket build(String hostname, int port, String sslProtocol)
-      throws NoSuchAlgorithmException, KeyManagementException, IOException {
+  public SSLSocket build(
+      String hostname,
+      InetAddress address,
+      Integer port,
+      InetAddress localAddress,
+      Integer localPort, String sslProtocol)
+      throws NoSuchAlgorithmException,
+      KeyManagementException,
+      IOException,
+      IllegalArgumentException {
     SSLContext sslContext = SSLContext.getInstance(sslProtocol);
     sslContext.init(this.keyManagerList, this.trustManagerList, null);
 
     SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
 
-    SSLSocket sslSocket =
-        (SSLSocket) sslSocketFactory.createSocket(hostname, port);
+    SSLSocket sslSocket = null;
+
+    if (hostname == null
+        && address == null
+        && port == null
+        && localAddress == null
+        && localPort == null) {
+      sslSocket = (SSLSocket) sslSocketFactory.createSocket();
+    } else if (hostname == null
+        && address != null
+        && port != null
+        && localAddress == null
+        && localPort == null) {
+      sslSocket = (SSLSocket) sslSocketFactory.createSocket(address, port);
+    } else if (hostname == null
+        && address != null
+        && port != null
+        && localAddress != null
+        && localPort != null) {
+      sslSocket = (SSLSocket) sslSocketFactory
+          .createSocket(address, port, localAddress, localPort);
+    } else if (hostname != null
+        && address == null
+        && port != null
+        && localAddress == null
+        && localPort == null) {
+      sslSocket = (SSLSocket) sslSocketFactory.createSocket(hostname, port);
+    } else if (hostname != null
+        && address == null
+        && port != null
+        && localAddress != null
+        && localPort != null) {
+      sslSocket = (SSLSocket) sslSocketFactory
+          .createSocket(hostname, port, localAddress, localPort);
+    } else {
+      throw new IllegalArgumentException();
+    }
+
     sslSocket.setEnabledCipherSuites(sslSocket.getSupportedCipherSuites());
 
     return sslSocket;
   }
 
+  public SSLSocket build()
+      throws NoSuchAlgorithmException, KeyManagementException, IOException {
+    return this.build(null, null, null, null, null, TLS);
+  }
+
+  public SSLSocket build(InetAddress address, int port)
+      throws NoSuchAlgorithmException, KeyManagementException, IOException {
+    return this.build(null, address, port, null, null, TLS);
+  }
+
+  public SSLSocket build(
+      InetAddress address, int port, InetAddress localAddress, int localPort)
+      throws NoSuchAlgorithmException, KeyManagementException, IOException {
+    return this.build(null, address, port, localAddress, localPort, TLS);
+  }
+
   public SSLSocket build(String hostname, int port)
       throws NoSuchAlgorithmException, KeyManagementException, IOException {
-    return this.build(hostname, port, TLS);
+    return this.build(hostname, null, port, null, null, TLS);
+  }
+
+  public SSLSocket build(
+      String hostname, int port, InetAddress localAddress, int localPort)
+      throws NoSuchAlgorithmException, KeyManagementException, IOException {
+    return this.build(hostname, null, port, localAddress, localPort, TLS);
   }
 }
